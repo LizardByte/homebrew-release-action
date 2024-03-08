f5c140acba8d5ad2ea6fe46fca790843cf54e3f8
ReenigneArcher <42013603+ReenigneArcher@users.noreply.github.com>
2024-03-08 15:51:19 -0500

    contribute to homebrew-core

--- README.md
+++ README.md
@@ -12,14 +12,18 @@ The intent here is that the formulas are built in the upstream repository, inste
 
 As part of an automated release workflow, this action can be used to upload the built formulas to the tap repository.
 
+Additionally, this action can be used to contribute the formula to an upstream homebrew-core repository, by
+creating a pull request to the upstream repository.
+
 ```yaml
 steps:
   - name: Publish Homebrew Formula
     uses: LizardByte/homebrew-release-action@master
     with:
+      formula_file: "${{ github.workspace }}/hello_world.rb"
       git_email: ${{ secrets.GIT_EMAIL }}
       git_username: ${{ secrets.GIT_USERNAME }}
-      target_repo: repo_owner/repo_name
+      org_homebrew_repo: repo_owner/repo_name
       token: ${{ secrets.PAT }}
 ```
 
@@ -30,10 +34,13 @@ steps:
   - name: Publish Homebrew Formula
     uses: LizardByte/homebrew-release-action@master
     with:
-      local_repo_directory: "${{ github.workspace }}/homebrew-repo"
+      contribute_to_homebrew_core: true
+      formula_file: "${{ github.workspace }}/hello_world.rb"
       git_email: ${{ secrets.GIT_EMAIL }}
       git_username: ${{ secrets.GIT_USERNAME }}
-      target_repo: repo_owner/repo_name
-      target_repo_branch: master
+      homebrew_core_fork_repo: repo_owner/homebrew-core
+      org_homebrew_repo: repo_owner/repo_name
+      org_homebrew_repo_branch: master
       token: ${{ secrets.PAT }}
+      upstream_homebrew_core_repo: Homebrew/homebrew-core
 ```
--- action/main.py
+++ action/main.py
@@ -36,24 +36,54 @@ def _parse_args(args_list: list) -> argparse.Namespace:
 def _run_subprocess(
         args_list: list,
         capture_output: bool = False,
+        cwd: Optional[str] = None,
         env: Optional[Mapping] = None,
 ) -> bool:
     global ERROR
     result = subprocess.run(
         args=args_list,
         capture_output=capture_output,
+        cwd=cwd,
         env=env,
     )
-    print('Captured stdout:\n\n', result.stdout.decode('utf-8') if result.stdout else '')
+    print('Captured stdout:\n\n')
+    print(result.stdout.decode('utf-8') if result.stdout else '')
 
     try:
         result.check_returncode()
     except subprocess.CalledProcessError:
         ERROR = True
-        print('Captured stderr:\n\n', result.stderr.decode('utf-8') if result.stderr else '')
+        print('Captured stderr:\n\n')
+        print(result.stderr.decode('utf-8') if result.stderr else '')
         return False
     else:
         return True
+
+
+def set_github_action_output(output_name: str, output_value: str):
+    """
+    Set the output value by writing to the outputs in the Environment File, mimicking the behavior defined <here
+    https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-output-parameter>__.
+
+    Parameters
+    ----------
+    output_name : str
+        Name of the output.
+    output_value : str
+        Value of the output.
+    """
+    with open(os.path.abspath(os.environ["GITHUB_OUTPUT"]), "a") as f:
+        f.write(f'{output_name}<<EOF\n')
+        f.write(output_value)
+        f.write('\nEOF\n')
+
+
+def branch_exists(branch: str) -> bool:
+    try:
+        subprocess.check_output(['git', 'show-ref', '--verify', '--quiet', f'refs/heads/{branch}'])
+        return True
+    except subprocess.CalledProcessError:
+        return False
 
 
 def get_brew_repository() -> str:
@@ -62,6 +92,65 @@ def get_brew_repository() -> str:
         capture_output=True,
     )
     return proc.stdout.decode('utf-8').strip()
+
+
+def prepare_homebrew_core_fork(
+        branch_suffix: str,
+        path: str,
+) -> None:
+    print('Preparing Homebrew/homebrew-core fork')
+
+    # checkout a new branch
+    branch_name = f'homebrew-release-action/{branch_suffix}'
+    if not branch_exists(branch_name):  # create a new branch if it does not exist
+        print(f'Creating new branch {branch_name}')
+        _run_subprocess(
+            args_list=['git', 'checkout', '-b', branch_name],
+            capture_output=True,
+            cwd=path,
+        )
+    else:  # checkout the existing branch
+        print(f'Checking out existing branch {branch_name}')
+        _run_subprocess(
+            args_list=['git', 'checkout', branch_name],
+            capture_output=True,
+            cwd=path,
+        )
+
+    # add the upstream remote
+    print('Adding upstream remote')
+    _run_subprocess(
+        args_list=[
+            'git',
+            'remote',
+            'add',
+            'upstream',
+            f'https://github.com/{os.environ["INPUT_UPSTREAM_HOMEBREW_CORE_REPO"]}'
+        ],
+        capture_output=True,
+        cwd=path,
+    )
+
+    # fetch the upstream remote
+    print('Fetching upstream remote')
+    _run_subprocess(
+        args_list=['git', 'fetch', 'upstream'],
+        capture_output=True,
+        cwd=path,
+    )
+
+    # hard reset
+    print('Hard resetting to upstream/master')
+    _run_subprocess(
+        args_list=['git', 'reset', '--hard', 'upstream/master'],
+        capture_output=True,
+        cwd=path,
+    )
+
+    set_github_action_output(
+        output_name='homebrew_core_branch',
+        output_value=branch_name
+    )
 
 
 def process_input_formula(formula_file: str) -> str:
@@ -80,6 +169,8 @@ def process_input_formula(formula_file: str) -> str:
     # get filename
     formula_filename = os.path.basename(formula_file)
     print(f'formula_filename: {formula_filename}')
+
+    formula = formula_filename.split('.')[0]
 
     # get the first letter of formula name
     first_letter = formula_filename[0].lower()
@@ -108,12 +199,18 @@ def process_input_formula(formula_file: str) -> str:
         capture_output=True
     )
 
-    clone_repo = os.path.join(os.environ['GITHUB_ACTION_PATH'], 'build')
-    print(f'clone_repo: {clone_repo}')
+    org_homebrew_repo = os.path.join(os.environ['GITHUB_ACTION_PATH'], 'org_homebrew_repo')
+    homebrew_core_fork_repo = os.path.join(os.environ['GITHUB_ACTION_PATH'], 'homebrew_core_fork_repo')
+    print(f'org_homebrew_repo: {org_homebrew_repo}')
+    print(f'homebrew_core_fork_repo: {homebrew_core_fork_repo}')
+
+    if os.getenv('INPUT_CONTRIBUTE_TO_HOMEBREW_CORE').lower() == 'true':
+        prepare_homebrew_core_fork(branch_suffix=formula, path=homebrew_core_fork_repo)
 
     # copy the formula file to the two directories
     tap_dirs = [
-        os.path.join(clone_repo, 'Formula', first_letter),  # this is the repo we will push back to
+        os.path.join(org_homebrew_repo, 'Formula', first_letter),  # we will commit back to this
+        os.path.join(homebrew_core_fork_repo, 'Formula', first_letter),  # we will commit back to this
         os.path.join(get_brew_repository(), 'Library', 'Taps', temp_repo, 'Formula', first_letter)  # tap directory
     ]
     for d in tap_dirs:
@@ -125,7 +222,7 @@ def process_input_formula(formula_file: str) -> str:
             raise FileNotFoundError(f'::error:: Formula file {formula_filename} was not copied to {d}')
         print(f'Copied {formula_filename} to {d}')
 
-    return formula_filename.split('.')[0]
+    return formula
 
 
 def is_brew_installed() -> bool:
